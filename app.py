f6ee8e0b58199861ce39fe9170dbf43c45918d24
TatsanaSrisawang <64346499+TNPKpenguin@users.noreply.github.com>
2024-06-26 18:04:25 +0700

    Add files via upload
    create new function that can add the image to firebase

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 from flask import Flask, render_template, request
-import flask
+import firebase_admin
+from firebase_admin import credentials, storage
+import datetime 
+
+cred_path  = r"C:\Users\LENOVO\Desktop\HCU\summer_4_1\deploy_models_test\imagestestapi-firebase-adminsdk-rthos-548c10df85.json"
+
+cred = credentials.Certificate(cred_path)
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'imagestestapi.appspot.com'
+})
+
+bucket = storage.bucket()
+
+def upload_image(image_path, destination_blob_name):
+    """Uploads an image to Firebase Storage."""
+    # Create a blob in the bucket
+    blob = bucket.blob(destination_blob_name)
+
+    # Upload the image
+    blob.upload_from_filename(image_path)
+
+    # Make the blob publicly viewable (optional)
+    blob.make_public()
+
+    print(f"File uploaded to {blob.public_url}")
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -10,7 +35,8 @@ def hello_world():
 def predict():
     imageFile = request.files['imagefile']
     image_path = "./images/" + imageFile.filename
-    imageFile.save(image_path)
+    # imageFile.save(image_path)
+    upload_image(image_path, f"images/{str(datetime.datetime.now())}.png")
 
     classification = ""
     if imageFile.filename == "test.png":
